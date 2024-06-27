d3049d22058ad30086059393796945fa62091262
sbauersfeld <sbauersfeld>
2024-06-27 02:14:45 +0000

    demoext

--- notebook.py
+++ notebook.py
@@ -4,3 +4,59 @@
 # COMMAND ----------
 
 2+2
+
+# COMMAND ----------
+
+# MAGIC %sh cat notebook
+
+# COMMAND ----------
+
+abc
+
+# COMMAND ----------
+
+# MAGIC %ls -l
+
+# COMMAND ----------
+
+1+8
+
+# COMMAND ----------
+
+# MAGIC %sh cat /databricks/runtime/info.json
+
+# COMMAND ----------
+
+# MAGIC %sh ls
+
+# COMMAND ----------
+
+print(spark.conf.get("spark.databricks.clusterUsageTags.clusterId"))
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l
+
+# COMMAND ----------
+
+# MAGIC %sh cat rmdnb.r
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l
+
+# COMMAND ----------
+
+# MAGIC %run notebook.ipynb
+
+# COMMAND ----------
+
+# MAGIC %sh cat ipynbnb.ipynb
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l
+
+# COMMAND ----------
+
+
